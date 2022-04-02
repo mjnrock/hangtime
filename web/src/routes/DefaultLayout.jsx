a7aa84a9fd8dd4af9a659f0f3a82d9b2b7dfe09e
LesPantsFancy <mjnrock@umich.edu>
2022-04-02 15:26:33 -0400

    init

--- web/src/routes/DefaultLayout.jsx
+++ web/src/routes/DefaultLayout.jsx
@@ -3,20 +3,12 @@ import { Outlet, Link } from "react-router-dom";
 export function DefaultLayout() {
 	return (
 		<div>
-			<div className="text-2xl">Header</div>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/host">Host</Link>
-					</li>
-					<li>
-						<Link to="/find">Find</Link>
-					</li>
-				</ul>
-			</nav>
+			<div className="text-2xl font-bold font-mono p-2 text-center">Hangtime</div>
+			<div className="flex flex-row text-center font-bold">
+				<Link className="flex-1 hover:bg-gray-700 hover:text-white p-5" to="/">Home</Link>
+				<Link className="flex-1 hover:bg-gray-700 hover:text-white p-5" to="/host">Host</Link>
+				<Link className="flex-1 hover:bg-gray-700 hover:text-white p-5" to="/find">Find</Link>
+			</div>
 
 			<hr />
 
