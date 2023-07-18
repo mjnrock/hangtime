759a48bbc88b6f1931d3840a70e60b390b4d93e1
LesPantsFancy <mjnrock@umich.edu>
2023-07-18 09:20:32 -0400

    init

--- client/src/routes/Scoreboard.jsx
+++ client/src/routes/Scoreboard.jsx
@@ -59,68 +59,80 @@ export function Scoreboard() {
 	};
 
 	return (
-		<div className="flex flex-col items-center justify-center">
-			<h1>Scoreboard</h1>
+		<div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+			<h1 className="text-2xl font-semibold text-gray-900">Scoreboard</h1>
 
-			<div className="flex flex-row items-center justify-center">
-				{ teams.map((team, teamIndex) => (
-					<div className="flex flex-col items-center justify-center" key={ teamIndex }>
-						<div className="flex flex-row items-center justify-center">
-							{ teams.length > 1 && (
-								<button
-									className="px-4 py-2 m-2 text-white bg-gray-500 rounded shadow-sm hover:bg-gray-600 active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-									onClick={ () => removeTeam(teamIndex) }
-								>
-									Remove Team
-								</button>
-							) }
-						</div>
-						{ team.name }
-						<div>Total Team Score: { team.players.reduce((totalScore, player) => totalScore + player.score, 0) }</div>
-						{ team.players.map((player, playerIndex) => (
-							<div key={ playerIndex }>
-								{ player.name }
-								<div>Score: { player.score }</div>
-								<div className="flex flex-row items-center justify-center">
+			{ teams.map((team, teamIndex) => (
+				<div key={ teamIndex } className="grid grid-cols-1 gap-5 mt-5 sm:grid-cols-2 lg:grid-cols-3">
+					<div className="overflow-hidden bg-white rounded-lg shadow">
+						<div className="px-4 py-5 sm:p-6">
+							<div className="flex items-center justify-between">
+								<h3 className="text-lg font-medium leading-6 text-gray-900">{ team.name }</h3>
+								{ teams.length > 1 && (
 									<button
-										className="px-4 py-2 m-2 text-white bg-green-500 rounded shadow-sm hover:bg-green-600 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-										onClick={ () => incrementScore(teamIndex, playerIndex) }
+										className="px-4 py-2 text-white bg-gray-500 rounded shadow-sm hover:bg-gray-600 active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+										onClick={ () => removeTeam(teamIndex) }
 									>
-										+
-									</button>
-									<button
-										className="px-4 py-2 m-2 text-white bg-red-500 rounded shadow-sm hover:bg-red-600 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-										onClick={ () => decrementScore(teamIndex, playerIndex) }
-									>
-										-
-									</button>
-								</div>
-								{ team.players.length > 1 && (
-									<button
-										className="px-4 py-2 m-2 text-white bg-gray-500 rounded shadow-sm hover:bg-gray-600 active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-										onClick={ () => removePlayer(teamIndex, playerIndex) }
-									>
-										Remove Player
+										Remove Team
 									</button>
 								) }
 							</div>
-						)) }
-						<button
-							className="px-4 py-2 m-2 text-white bg-blue-500 rounded shadow-sm hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-							onClick={ () => addPlayer(teamIndex) }
-						>
-							Add Player
-						</button>
+							<div className="max-w-xl mt-2 text-sm text-gray-500">
+								<p>Total Team Score: { team.players.reduce((totalScore, player) => totalScore + player.score, 0) }</p>
+							</div>
+							{ team.players.map((player, playerIndex) => (
+								<div key={ playerIndex } className="mt-5">
+									<div className="flex items-center justify-between">
+										<div>
+											<h5 className="text-lg font-medium leading-6 text-gray-900">{ player.name }</h5>
+											<div className="max-w-xl mt-2 text-sm text-gray-500">
+												<p>Score: { player.score }</p>
+											</div>
+										</div>
+										<div className="flex items-center justify-between space-x-4">
+											<button
+												className="px-4 py-2 text-white bg-green-500 rounded shadow-sm hover:bg-green-600 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+												onClick={ () => incrementScore(teamIndex, playerIndex) }
+											>
+												+
+											</button>
+											<button
+												className="px-4 py-2 text-white bg-red-500 rounded shadow-sm hover:bg-red-600 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+												onClick={ () => decrementScore(teamIndex, playerIndex) }
+											>
+												-
+											</button>
+											{ team.players.length > 1 && (
+												<button
+													className="px-4 py-2 text-white bg-gray-500 rounded shadow-sm hover:bg-gray-600 active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+													onClick={ () => removePlayer(teamIndex, playerIndex) }
+												>
+													Remove Player
+												</button>
+											) }
+										</div>
+									</div>
+								</div>
+							)) }
+							<button
+								className="px-4 py-2 mt-5 text-white bg-blue-500 rounded shadow-sm hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+								onClick={ () => addPlayer(teamIndex) }
+							>
+								Add Player
+							</button>
+						</div>
 					</div>
-				)) }
-			</div>
+				</div>
+			)) }
 
-			<button
-				className="px-4 py-2 mt-4 text-white bg-blue-500 rounded shadow-sm hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-				onClick={ addTeam }
-			>
-				Add Team
-			</button>
+			<div className="mt-5">
+				<button
+					className="px-4 py-2 text-white bg-blue-500 rounded shadow-sm hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+					onClick={ addTeam }
+				>
+					Add Team
+				</button>
+			</div>
 		</div>
 	);
 }
